43e420f20aa89579477fc461595d62fbd6818809
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2021-08-11 18:09:37 +0200

    Fix in_ parameters.

--- faim_sl_tasks/img/intensity/SingleChannelIlluminationFieldCorrectionTask.py
+++ faim_sl_tasks/img/intensity/SingleChannelIlluminationFieldCorrectionTask.py
@@ -6,20 +6,17 @@ from faim_sl.MultiFileTask import MultiFileTask
 
 class SingleChannelIlluminationFieldCorrectionTask(MultiFileTask):
     # Params
-    illumination_field_path = None
-    illumination_field = None
+    in_illumination_field = None
 
     scale = luigi.IntParameter(default=255)
 
-    def prepare_inputs(self):
-        self.illumination_field_path = self.in_data['illumination_field']().path
-        self.in_data = self.in_data['batch']
+    ill_field_img = None
 
     def prepare(self):
-        self.illumination_field = imread(self.illumination_field_path)
+        self.ill_field_img = imread(self.in_illumination_field().path)
 
     def run_computation(self, img):
-        return self.compute(img, illumination_field=self.illumination_field, scale=self.scale)
+        return self.compute(img, illumination_field=self.ill_field_img, scale=self.scale)
 
     @staticmethod
     def compute(img, illumination_field, scale):
